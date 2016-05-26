4bc6839345296ea5b5a5011803599fd8e86df4b5
Alan Schoen <alan.schoen@gmail.com>
2016-05-26 17:34:37 -0400

    Compute answers to python q5

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,30 @@
-# Hint:  use Google to find python function
+from datetime import datetime
+
+pf = "%m/%d/%y"
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+
+pattern = "%m-%d-%Y"
+d1 = datetime.strptime(date_start, pattern)
+d2 = datetime.strptime(date_stop, pattern)
+print "%d days from %s to %s"%((d2-d1).days, d1.strftime(pf), d2.strftime(pf))
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+
+pattern = "%m%d%Y"
+d1 = datetime.strptime(date_start, pattern)
+d2 = datetime.strptime(date_stop, pattern)
+print "%d days from %s to %s"%((d2-d1).days, d1.strftime(pf), d2.strftime(pf))
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_start = '15-Jan-1994' 
+date_stop = '14-Jul-2015'
+
+pattern = "%d-%b-%Y"
+d1 = datetime.strptime(date_start, pattern)
+d2 = datetime.strptime(date_stop, pattern)
+print "%d days from %s to %s"%((d2-d1).days, d1.strftime(pf), d2.strftime(pf))
