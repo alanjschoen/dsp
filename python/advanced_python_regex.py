5b19a64439aa4994b0a73e2aff812d36aa7e4b09
Alan Schoen <alan.schoen@gmail.com>
2016-06-09 20:18:23 -0400

    Solved advanced_python_regex

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,23 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+import numpy as np
+
+# Read CSV
+df = pd.read_csv('faculty.csv', sep=',', skipinitialspace=True)
+
+# Get list of all degrees
+degrees_raw = [d for l in df['degree'].values for d in l.split()]
+
+# Convert to lower case and remove periods for matching
+degrees_low = [d.lower().replace('.', '') for d in degrees_raw]
+
+# Process each unique degree
+for d in set(degrees_low):
+
+    # Find all matching degrees in their original format
+    matches = [dr for (dl,dr) in zip(degrees_low, degrees_raw) if dl==d]
+
+    # Find the most frequent format of this degree
+    d_fmt = max(set(matches), key=matches.count)
+
+    # Print degree with the count
+    print '%s: %d'%(d_fmt,len(matches))
