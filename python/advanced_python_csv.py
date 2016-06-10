644e16f31eabeb683b7a0d427317459c43fb1b19
Alan Schoen <alan.schoen@gmail.com>
2016-06-10 00:11:48 -0400

    Update advanced python Q5: CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -6,6 +6,7 @@ text = open("faculty.csv","r").read()
 # Find all emails
 emails = re.finditer("([\w.]*@[\w.]*)", text)
 
+# Write CSV file
 file_out = open('emails.csv', 'w')
 for e in emails:
     file_out.write("%s\n"%e.group())
