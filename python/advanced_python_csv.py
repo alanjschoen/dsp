e4e32c530327b837ec5d2070f34405a39c3d89f7
Alan Schoen <alan.schoen@gmail.com>
2016-06-09 23:39:27 -0400

    Solve advanced python Q5: CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+import re
+
+# Open file as plain text
+text = open("faculty.csv","r").read()
+
+# Find all emails
+emails = re.finditer("([\w.]*@[\w.]*)", text)
+
+file_out = open('emails.csv', 'w')
+for e in emails:
+    file_out.write("%s\n"%e.group())
+file_out.close()
