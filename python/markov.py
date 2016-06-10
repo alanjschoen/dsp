9ef2968f9af1f3123c8d3f93ff545a4ee4372888
Alan Schoen <alan.schoen@gmail.com>
2016-06-10 12:33:42 -0400

    Markov version 4

--- python/markov.py
+++ python/markov.py
@@ -14,67 +14,78 @@
 
 from random import randint
 import string
+import sys
 
-printable = set(string.printable)
+def get_input():
+    # fname = "text_samples/neuromancer.txt"
+    # fname = "text_samples/wasteland.txt"
+    # fname = "text_samples/cat_in_hat.txt"
+    # fname = "text_samples/green_eggs_ham.txt"
+    # fname = "text_samples/obama.txt"
+    fname = sys.argv[1]
 
-fname = "text_samples/wasteland.txt"
-fname = "text_samples/cat_in_hat.txt"
-fname = "text_samples/green_eggs_ham.txt"
-fname = "text_samples/neuromancer.txt"
+    #output_len = 20
+    output_len = int(sys.argv[2])
 
+    return (fname, output_len)
 
-#fname = raw_input()
+def main():
+    (fname, output_len) = get_input()
+    text = open(fname, 'r').read()
+    text = clean(text)
+    markov_chain_length = 3
 
-output_len = 50
-#output_len = input()
+    # Simulate markov speech
+    (output, nbranches) = doMarkov(text, output_len, markov_chain_length)
 
-chain_n = 3
+    print ' '.join(output)
 
-text = open(fname, 'r').read()
-text = text.lower()
-#text = ''.join([let if let in printable else ' ' for let in text])
-text = text.replace("'", "")
-text = text.replace("`", "")
-text = text.replace("-", "")
+def doMarkov(text, output_len, chain_n):
+    # Get all words
+    words = text.split()
 
+    ### PART 1: Start the text ###
+    # Choose a random place in the text
+    randno = randint(0,len(words)-chain_n)
+    output = words[randno:randno+chain_n]
 
-# Get all words
-words = text.split()
+    ### PART 2: generate the rest ###
+    # Keep track of the number of times the code actually made a decision
+    nbranches = 0
 
-### PART 1: Start the text ###
-randno = randint(0,len(words)-chain_n)
-output = words[randno:randno+chain_n]
+    # Find all occurences of this string of words, and collect the next word
+    while len(output) < output_len:
+        state = output[-chain_n:]
+        history = []
 
-### PART 2: generate the rest ###
+        # Collect stats on past states like this
+        validCount = 0
+        for i in range(len(words)-chain_n-1):
+            isValid = True
+            for j in range(chain_n):
+                isValid &= state[j]==words[i+j]
+            if isValid:
+                history.append(words[i+chain_n])
 
-# Keep track of the number of times the code actually made a decision
-nbranches = 0
+        # Keep track of the number of times the simulation made a choice
+        if len(history) > 1:
+            nbranches += 1
 
-while len(output) < output_len:
-    state = output[-chain_n:]
-    history = []
+        # Pick a next word at random from the list
+        randno = randint(0, len(history)-1)  
+        output.append(history[randno])
 
-    # Collect stats on past states like this
-    validCount = 0
-    for i in range(len(words)-chain_n-1):
-        isValid = True
-        for j in range(chain_n):
-            isValid &= state[j]==words[i+j]
-        if isValid:
-            history.append(words[i+chain_n])
+    return (output, nbranches)
 
-    # Check if this string is unique
-    if len(history) > 1:
-        nbranches += 1
+# I tested several methods of cleaning the text
+def clean(text):
+    text.replace('\n', ' ')
+    #text = text.lower()
+    #text = ''.join([let if let in string.printable else ' ' for let in text])
+    #letset = string.letters + ".-,!'?"
+    #text = ''.join([let if let in letset else ' ' for let in text])
+    return text
 
-    # Make a random choice
-    randno = randint(0, len(history)-1)  
-    output.append(history[randno])
-
-# Print output
-print ' '.join(output)
-print nbranches
-
-
-
+if __name__ == '__main__':
+    main()
 
