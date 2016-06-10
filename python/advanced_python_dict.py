386d6abc627c6f001ecc164c9e155d8881044be9
Alan Schoen <alan.schoen@gmail.com>
2016-06-10 00:12:47 -0400

    Solve advanced python dict Q6

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+import re
+
+### Question 1
+print "\nQ1"
+
+f_in = open('faculty.csv', 'r')
+f_in.next()
+
+faculty = dict()
+for line in f_in:
+    line = [w.strip() for w in line.split(',')]
+    lastname = line[0].split()[-1]
+    degree = line[1]
+    title = re.match('.*Professor', line[2]).group()
+    email = line[3]
+
+    value = [degree, title, email]
+    if lastname in faculty:
+        faculty[lastname] += [value]
+    else:
+        faculty[lastname] = [value]
+f_in.close()
+
+for k in faculty.keys()[:4]:
+    print "%s: %s  "%(k,faculty[k])
+
