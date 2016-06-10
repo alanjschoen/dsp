520aa872736fbc7de79484775e35bd95692739d1
Alan Schoen <alan.schoen@gmail.com>
2016-06-10 00:23:35 -0400

    Solve advanced python dict Q7

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,26 +1,73 @@
 import re
 
-### Question 1
-print "\nQ1"
+### Question 6
+print "\nQ6"
 
+# Open CSV
 f_in = open('faculty.csv', 'r')
+
+# Skip first line
 f_in.next()
 
 faculty = dict()
 for line in f_in:
+
+    # Split line into fields
     line = [w.strip() for w in line.split(',')]
+
+    # Extract info
     lastname = line[0].split()[-1]
     degree = line[1]
     title = re.match('.*Professor', line[2]).group()
     email = line[3]
 
+    # Add data to dictionary
     value = [degree, title, email]
     if lastname in faculty:
         faculty[lastname] += [value]
     else:
         faculty[lastname] = [value]
+
+# Close file
 f_in.close()
 
-for k in faculty.keys()[:4]:
+# Print first 3 entries
+for k in faculty.keys()[:3]:
     print "%s: %s  "%(k,faculty[k])
 
+### Question 7
+print "\nQ7"
+
+# Open CSV
+f_in = open('faculty.csv', 'r')
+
+# Skip first line
+f_in.next()
+
+faculty = dict()
+for line in f_in:
+
+    # Split line into fields
+    line = [w.strip() for w in line.split(',')]
+
+    # Get first name and last name
+    ind = line[0].rfind(' ')
+    lastname = line[0][ind+1:]
+    firstname = line[0][:ind]
+
+    # Get degree
+    degree = line[1]
+    title = re.match('.*Professor', line[2]).group()
+    email = line[3]
+
+    # Add entry to dictionary
+    key = (lastname, firstname)
+    value = [degree, title, email]
+    faculty[key] = value
+
+# Close file
+f_in.close()
+
+# Print first 3 entries
+for k in faculty.keys()[:3]:
+    print "%s: %s"%(k,faculty[k])
