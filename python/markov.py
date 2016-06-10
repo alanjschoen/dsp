7d2259cdfda748bf9fa1b725c0e8353b113e220e
Alan Schoen <alan.schoen@gmail.com>
2016-06-10 02:52:52 -0400

    Markov version 1

--- python/markov.py
+++ python/markov.py
@@ -12,4 +12,91 @@
 
 # There are design choices to make; feel free to experiment and shape the program as you see fit. Jeff Atwood's [Markov and You](http://blog.codinghorror.com/markov-and-you/) is a fun place to get started learning about what you're trying to make.
 
-> REPLACE THIS TEXT WITH YOUR PROGRAM
+from random import randint
+import string
+
+printable = set(string.printable)
+
+
+fname = "text_samples/cat_in_hat.txt"
+fname = "text_samples/neuromancer.txt"
+fname = "text_samples/green_eggs_ham.txt"
+fname = "text_samples/wasteland.txt"
+#fname = raw_input()
+
+output_len = 60
+#output_len = input()
+
+chain_n = 4
+
+text = open(fname, 'r').read()
+text = ''.join([let if let in printable else ' ' for let in text])
+text = text.replace("'", "")
+text = text.replace("`", "")
+text = text.replace("-", "")
+
+### PART 1: Start the text ###
+# Divide text into lines to find most common strings of words that start new lines
+openers = dict()
+lines = text.split('\n')
+validCount = 0
+for l in lines:
+    words = l.split()
+    if len(words) >= chain_n:
+        op = tuple(words[:chain_n])
+        if op in openers:
+            openers[op] += 1
+        else:
+            openers[op] = 1
+        validCount += 1
+
+# Pick first few words from most common line openers
+randno = randint(1, validCount)
+pos = 0
+for op in openers.keys():
+    pos += openers[op]
+    if pos >= randno:
+        output = list(op)
+        break
+
+### PART 2: generate the rest ###
+# Get all words
+words = text.split()
+
+while len(output) < output_len:
+    state = output[-chain_n:]
+    history = dict()
+
+    # Collect stats on past states like this
+    validCount = 0
+    for i in range(0,len(words)-chain_n-1):
+        isValid = True
+        for j in range(chain_n):
+            isValid &= state[j]==words[i+j]
+        if isValid:
+            nextWord = words[i+chain_n]
+            if nextWord in history:
+                history[words[i+chain_n]] += 1
+            else:
+                history[words[i+chain_n]] = 1
+            validCount += 1
+
+    # Choose random number
+    randno = randint(1, validCount)
+
+    pastWords = history.keys()
+    pos = 0
+    for w in pastWords:
+        pos += history[w]
+        if pos >= randno:
+            nextWord = w
+            break
+
+    output.append(nextWord)
+
+# Print output
+print ' '.join(output)
+
+
+
+
